59a7248edfd05b31ddd71e627a015ac3f6ae7622
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-04-29 22:50:13 +0000

    add login & SignUp simple UI

--- client/src/screens/Login.js
+++ client/src/screens/Login.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return(
-        <h1>Login</h1>
-    )
-}
+	return (
+		<div className="mycard">
+			<div className="card auth-card input-field">
+				<h2>Instagram Clone</h2>
+				<input type="text" placeholder="email" />
+				<input type="text" placeholder="password" />
+				<button className="btn waves-effect waves-light">
+					Login
+				</button>
+				<h5>
+					<Link to="/signup">
+						Does not have an account ? Create one
+					</Link>
+				</h5>
+			</div>
+		</div>
+	);
+};
 
 export default Login;
--- client/src/screens/Signup.js
+++ client/src/screens/Signup.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-    return(
-        <h1>Signup</h1>
-    )
-}
+	return (
+		<div className="mycard">
+			<div className="card auth-card input-field">
+				<h2>Instagram Clone</h2>
+				<input type="text" placeholder="name" />
+				<input type="text" placeholder="email" />
+				<input type="text" placeholder="password" />
+				<button className="btn waves-effect waves-light">
+					Sign Up
+				</button>
+				<h5>
+					<Link to="/login">
+						Already have an account ? Login
+					</Link>
+				</h5>
+			</div>
+		</div>
+	);
+};
 
 export default Signup;
