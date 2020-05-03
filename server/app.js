04771fdb6e97296b9436215f5745f1ee4a0d5cfb
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-05-03 03:01:02 +0000

    Sync SignUp func with Backend
    adding also :
    - regex email validation
    - Custom PopUp messages (errors, success )

--- server/app.js
+++ server/app.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+var cors = require("cors");
 
 const app = express();
-const PORT = 5000 ;
-const URL = 'mongodb://localhost:27017/instadb';
+const PORT = 5000;
+const URL = "mongodb://localhost:27017/instadb";
 
-
-mongoose.connect(URL, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+mongoose.connect(URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Successfully connected to InstaDB')
+mongoose.connection.on("connected", () => {
+	console.log("Successfully connected to InstaDB");
 });
-mongoose.connection.on('error', (err) => {
-    console.log('error while connecting to InstaDB : ',err)
+mongoose.connection.on("error", (err) => {
+	console.log("error while connecting to InstaDB : ", err);
 });
 
-require('./models/user');
-require('./models/post');
+require("./models/user");
+require("./models/post");
 
+app.use(cors());
 app.use(express.json());
-app.use(require('./routes/auth'));
-app.use(require('./routes/post'));
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
 
-app.listen(PORT,() =>{
-    console.log("Server is running under port 5000 ...")
-})
+app.listen(PORT, () => {
+	console.log("Server is running under port 5000 ...");
+});
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const JWT_SECRET = require('../constants');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = require("../constants");
 //const loginMiddle = require('../middleware/loginMiddleware');
 
 const User = mongoose.model("User");
 const router = express.Router();
 
 // Our welcome msg from the API
-router.get('/',(req,res)=>{
-    res.send("Welcome to our InstaClone API");
+router.get("/", (req, res) => {
+	res.send("Welcome to our InstaClone API");
 });
 
 // Just to test that our middleware is working correctly
@@ -21,70 +21,72 @@ router.get('/protected',loginMiddle,(req,res)=>{
 */
 
 // Route to handle SignUp requests
-router.post('/signup',(req,res)=>{
-    const {name,email,password} = req.body;
-    // Verifying if one of the fields is Empty
-    if(!name || !password || !email){
-        return res.status(422).json({error :"Please submit all required field"});
-    }
-    // Else we search the user with the credentials submitted
-    User.findOne({Email: email})
-        .then((savedUser)=>{
-            // Verify if the user exist in the DB
-            if(savedUser){
-                return res.status(422).json({error :"This Email Is Already Used !"});
-            }
-            // We Hash the pwd before save into DB, more the number is high more it's more secure
-            bcrypt.hash(password,12)
-                .then(hashedPwd =>{
-                    const user = new User({
-                        Name: name,
-                        Email: email,
-                        Password: hashedPwd
-                    });
-                    // We save our new user to DB
-                    user.save()
-                        .then(user => {
-                            res.json({message:'Saved successfully '})
-                        })
-                        .catch(err =>{
-                            console.log(err)
-                        })
-                });
-        })
-        .catch(err => {
-            console.log(err)
-        });
+router.post("/signup", (req, res) => {
+	const { name, email, password } = req.body;
+	// Verifying if one of the fields is Empty
+	if (!name || !password || !email) {
+		return res.json({ error: "Please submit all required field" });
+	}
+	// Else we search the user with the credentials submitted
+	User.findOne({ Email: email })
+		.then((savedUser) => {
+			// Verify if the user exist in the DB
+			if (savedUser) {
+				return res.json({ error: "This Email Is Already Used !" });
+			}
+			// We Hash the pwd before save into DB, more the number is high more it's more secure
+			bcrypt.hash(password, 12).then((hashedPwd) => {
+				const user = new User({
+					Name: name,
+					Email: email,
+					Password: hashedPwd,
+				});
+				// We save our new user to DB
+				user.save()
+					.then((user) => {
+						res.json({ message: "Saved successfully " });
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 // Route to handle SignIn requests
-router.post('/signin',(req,res) =>{
-    const {email,password} = req.body;
-    // Verification for an empty field
-    if(!email || !password){
-        return res.status(422).json({error :"Please provide Email or Password"});
-    }
-    // Check if email exist in our DB
-    User.findOne({Email:email})
-        .then(savedUser =>{
-            if(!savedUser){
-                return res.status(422).json({error:"Invalid Email or Password"});
-            }
-            bcrypt.compare(password,savedUser.Password)
-                .then(doMatch =>{
-                    if(doMatch){
-                        // we will generate the token based on the ID of user
-                        const token = jwt.sign({_id : savedUser._id},JWT_SECRET);
-                        res.json({token});
-                    }else{
-                        return res.status(422).json({error:"Invalid Email or Password"});
-                    }
-                })
-        })
-        .catch( err => {
-            console.log(err);
-        })
+router.post("/signin", (req, res) => {
+	const { email, password } = req.body;
+	// Verification for an empty field
+	if (!email || !password) {
+		return res.json({ error: "Please provide Email or Password" });
+	}
+	// Check if email exist in our DB
+	User.findOne({ Email: email })
+		.then((savedUser) => {
+			if (!savedUser) {
+				return res.json({ error: "Invalid Email or Password" });
+			}
+			bcrypt.compare(password, savedUser.Password).then((doMatch) => {
+				if (doMatch) {
+					// we will generate the token based on the ID of user
+					const token = jwt.sign(
+						{ _id: savedUser._id },
+						JWT_SECRET
+					);
+					res.json({ token });
+				} else {
+					return res.json({
+						error: "Invalid Email or Password",
+					});
+				}
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
-
-module.exports = router ;
+module.exports = router;
