4c3070ea2b79ecc7d7191a3dfb18656b0a7c0eeb
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-05-11 14:22:15 +0000

    Changing navigation link after SignIn/SignOut

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 const Navbar = () => {
+	const { state, dispatch } = useContext(UserContext);
+	const renderList = () => {
+		if (state) {
+			return [
+				<li>
+					<Link to="/profile">Profile</Link>
+				</li>,
+				<li>
+					<Link to="/create">Create Post</Link>
+				</li>,
+			];
+		} else {
+			return [
+				<li>
+					<Link to="/login">Login</Link>
+				</li>,
+				<li>
+					<Link to="/signup">SignUp</Link>
+				</li>,
+			];
+		}
+	};
 	return (
 		<nav>
 			<div className="nav-wrapper white">
-				<Link to="/" className="brand-logo">
+				<Link to={state ? "/" : "/login"} className="brand-logo">
 					Instagram Clone
 				</Link>
 				<ul id="nav-mobile" className="right">
-					<li>
-						<Link to="/login">Login</Link>
-					</li>
-					<li>
-						<Link to="/signup">SignUp</Link>
-					</li>
-					<li>
-						<Link to="/profile">Profile</Link>
-					</li>
-					<li>
-						<Link to="/create">Create Post</Link>
-					</li>
+					{renderList()}
 				</ul>
 			</div>
 		</nav>
