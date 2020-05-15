144a8348bbf05378762524f874ec9dab6cc2a996
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-05-15 10:54:44 +0000

    Implement Logout func

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../App";
 
 const Navbar = () => {
 	const { state, dispatch } = useContext(UserContext);
+	const history = useHistory();
 	const renderList = () => {
 		if (state) {
 			return [
@@ -12,6 +13,18 @@ const Navbar = () => {
 				</li>,
 				<li>
 					<Link to="/create">Create Post</Link>
+				</li>,
+				<li>
+					<button
+						className="btn waves-effect waves-light"
+						onClick={() => {
+							localStorage.clear();
+							dispatch({ type: "CLEAR" });
+							history.push("/login");
+						}}
+					>
+						LogOut
+					</button>
 				</li>,
 			];
 		} else {
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -3,5 +3,8 @@ export const reducer = (state, action) => {
 	if (action.type === "USER") {
 		return action.payload;
 	}
+	if (action.type === "CLEAR") {
+		return null;
+	}
 	return state;
 };
