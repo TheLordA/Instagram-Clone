4696a91833e4a9b9ebbd2a23b88e5b4170d6461d
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-05-20 14:44:52 +0000

    add follow/unfollow func on server side

--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 const userSchema = new mongoose.Schema({
-    Name:{
-        type:String, 
-        required:true
-    },
-    Email:{
-        type:String, 
-        required:true
-    },
-    Password:{
-        type:String, 
-        required:true
-    }
+	Name: {
+		type: String,
+		required: true,
+	},
+	Email: {
+		type: String,
+		required: true,
+	},
+	Password: {
+		type: String,
+		required: true,
+	},
+	Followers: [{ type: ObjectId, ref: "User" }],
+	Following: [{ type: ObjectId, ref: "User" }],
 });
 
-mongoose.model('User',userSchema);
+mongoose.model("User", userSchema);
--- server/routes/user.js
+++ server/routes/user.js
@@ -33,5 +33,54 @@ router.get("/user/:id", loginmiddleware, (req, res) => {
 			return res.status(404).json({ Error: "User not found" });
 		});
 });
+router.put("/follow", loginmiddleware, (req, res) => {
+	User.findByIdAndUpdate(
+		req.body.followId,
+		{
+			$push: { Followers: req.user._id },
+		},
+		{ new: true },
+		(err, result) => {
+			if (err) return res.status(422).json({ Error: err });
+
+			User.findByIdAndUpdate(
+				req.use._id,
+				{
+					$push: { Following: req.body.followId },
+				},
+				{ new: true }
+			);
+		}
+	)
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => res.status(422).json({ Error: err }));
+});
+
+router.put("/unfollow", loginmiddleware, (req, res) => {
+	User.findByIdAndUpdate(
+		req.body.unfollowId,
+		{
+			$pull: { Followers: req.user._id },
+		},
+		{ new: true },
+		(err, result) => {
+			if (err) return res.status(422).json({ Error: err });
+
+			User.findByIdAndUpdate(
+				req.use._id,
+				{
+					$pull: { Following: req.body.unfollowId },
+				},
+				{ new: true }
+			);
+		}
+	)
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((err) => res.status(422).json({ Error: err }));
+});
 
 module.exports = router;
