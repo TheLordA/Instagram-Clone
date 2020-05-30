d35e2b74b68e6da489b29cb409dd2d849a8ecd08
TheLordA <45141652+TheLordA@users.noreply.github.com>
2020-05-30 23:16:33 +0000

    add user Search func on server side
     And also sync it with the client side and display result to the client

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,15 +2,29 @@ import React, { useContext, useRef, useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { UserContext } from "../App";
 import M from "materialize-css";
+import Axios from "axios";
+import "./Navbar.css";
+
+//MaterialUI
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
 
 const Navbar = () => {
 	const { state, dispatch } = useContext(UserContext);
 	const history = useHistory();
 	const modalSearch = useRef(null);
-	const [search, setSearch] = useState("");
+	const [pattern, setPattern] = useState("");
+	const [search, setSearch] = useState([]);
+
 	useEffect(() => {
 		M.Modal.init(modalSearch.current);
 	}, []);
+
 	const renderList = () => {
 		if (state) {
 			return [
@@ -47,19 +61,52 @@ const Navbar = () => {
 			];
 		} else {
 			return [
-				<li key="1">
+				<li key="6">
 					<Link to="/login">Login</Link>
 				</li>,
-				<li key="2">
+				<li key="7">
 					<Link to="/signup">SignUp</Link>
 				</li>,
 			];
 		}
 	};
+
+	const FindUser = (pattern) => {
+		if (!(pattern === "")) {
+			const URL = `http://localhost:5000/users-research`;
+			const config = {
+				headers: {
+					Authorization: "Bearer " + localStorage.getItem("jwt"),
+				},
+			};
+			setPattern(pattern);
+			Axios.post(URL, { pattern }, config).then((res) => {
+				setSearch(res.data);
+			});
+		}
+	};
+
+	const _handleClick = () => {
+		M.Modal.getInstance(modalSearch.current).close();
+		setPattern("");
+		setSearch([]);
+	};
+
+	const useStyles = makeStyles((theme) => ({
+		root: {
+			width: "100%",
+		},
+		inline: {
+			display: "inline",
+		},
+	}));
+
+	const classes = useStyles();
+
 	return (
 		<nav>
 			<div className="nav-wrapper white">
-				<Link to={state ? "/" : "/login"} className="brand-logo">
+				<Link to={state ? "/" : "/login"} className="brand-logo" style={{ left: "10%" }}>
 					Instagram Clone
 				</Link>
 				<ul id="nav-mobile" className="right">
@@ -72,58 +119,40 @@ const Navbar = () => {
 					<input
 						type="text"
 						placeholder="Search"
-						value={search}
-						onChange={(e) => setSearch(e.target.value)}
+						value={pattern}
+						onChange={(e) => FindUser(e.target.value)}
 					/>
-					<ul className="collection">
-						<li className="collection-item avatar">
-							<img src="images/yuna.jpg" alt="" className="circle" />
-							<span className="title">Title</span>
-							<p>
-								First Line <br />
-								Second Line
-							</p>
-							<a href="#!" className="secondary-content">
-								<i className="material-icons">grade</i>
-							</a>
-						</li>
-						<li className="collection-item avatar">
-							<i className="material-icons circle">folder</i>
-							<span className="title">Title</span>
-							<p>
-								First Line <br />
-								Second Line
-							</p>
-							<a href="#!" className="secondary-content">
-								<i className="material-icons">grade</i>
-							</a>
-						</li>
-						<li className="collection-item avatar">
-							<i className="material-icons circle green">insert_chart</i>
-							<span className="title">Title</span>
-							<p>
-								First Line <br />
-								Second Line
-							</p>
-							<a href="#!" className="secondary-content">
-								<i className="material-icons">grade</i>
-							</a>
-						</li>
-						<li className="collection-item avatar">
-							<i className="material-icons circle red">play_arrow</i>
-							<span className="title">Title</span>
-							<p>
-								First Line <br />
-								Second Line
-							</p>
-							<a href="#!" className="secondary-content">
-								<i className="material-icons">grade</i>
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div className="modal-footer">
-					<button className="modal-close waves-effect waves-green btn-flat">Agree</button>
+
+					<List className={classes.root}>
+						{search.user
+							? search.user.map((item) => {
+									return (
+										<Link
+											key={item._id}
+											to={item._id !== state._id ? `/profile/${item._id}` : "/profile"}
+											onClick={() => _handleClick()}
+										>
+											<ListItem alignItems="flex-start">
+												<ListItemAvatar>
+													<Avatar
+														alt="Remy Sharp"
+														src="/static/images/avatar/1.jpg"
+													/>
+												</ListItemAvatar>
+
+												<ListItemText
+													primary={item.Name}
+													secondary={
+														<React.Fragment>{item.Email}</React.Fragment>
+													}
+												/>
+											</ListItem>
+											<Divider variant="inset" component="li" />
+										</Link>
+									);
+							  })
+							: "Is Loading ..."}
+					</List>
 				</div>
 			</div>
 		</nav>
--- server/routes/user.js
+++ server/routes/user.js
@@ -97,4 +97,26 @@ router.put("/unfollow", loginmiddleware, (req, res) => {
 	);
 });
 
+// Just Wrote the logic of it but not yet tested and the client implementation doesn't exist yet
+router.put("/update-picture", loginmiddleware, (req, res) => {
+	User.findByIdAndUpdate(req.user._id, { $set: { pic: req.body.pic } }, { new: true }, (err, result) => {
+		if (err) {
+			return res.status(422).json({ error: "pic canot post" });
+		}
+		res.json(result);
+	});
+});
+
+router.post("/users-research", (req, res) => {
+	let pattern = new RegExp("^" + req.body.pattern);
+	User.find({ Email: { $regex: pattern } })
+		.select("_id Email Name")
+		.then((user) => {
+			res.json({ user });
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
 module.exports = router;
